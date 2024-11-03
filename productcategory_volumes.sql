7651a8199b7d9ccd36eec33da9dc596acaef6ab5
ZoeyZYUUU <zheng_ying@student.ceu.edu>
2024-11-03 19:53:08 +0100

    Forgot the month

--- productcategory_volumes.sql
+++ productcategory_volumes.sql
@@ -1,7 +1,9 @@
-SELECT p.product_category_name AS Category,
+SELECT 
+    p.product_category_name AS Category,
+    DATE_FORMAT(o.order_purchase_timestamp, '%Y-%m') AS Month,
     COUNT(oi.order_id) AS Sales_Volume
 FROM order_items_dataset AS oi
 JOIN orders_dataset AS o ON oi.order_id = o.order_id
 JOIN products_dataset AS p ON oi.product_id = p.product_id
-GROUP BY Category
+GROUP BY Category, Month
 ORDER BY Sales_Volume DESC;
